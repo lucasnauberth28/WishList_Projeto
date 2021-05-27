89b5b175ad7c69e869c35df5a4a32c3d043c8c2a
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2021-05-27 15:56:59 -0300

    Banco de dados arrumado

--- Wishlist_Database_Scripts/DMLQuery3.sql
+++ Wishlist_Database_Scripts/DMLQuery3.sql
@@ -1,15 +1,19 @@
 USE Wish
 GO
 
-INSERT INTO Usuario (idDesejo, nome, email, senha)
-VALUES				(2, 'Caic','caic@hotmail.com','caic@caic'),
-					(1, 'Marco','marco@hotmail.com','marco@marco'),
-					(3, 'Nauberth','nauberth@hotmail.com','nauberth@nauberth'),
-					(4, 'Strilicherk','strili@hotmail.com','strili@strili')
+INSERT INTO Usuario (nome, email, senha)
+VALUES				('Caic','caic@hotmail.com','caic@caic'),
+					('Marco','marco@hotmail.com','marco@marco'),
+					('Nauberth','nauberth@hotmail.com','nauberth@nauberth'),
+					('Strilicherk','strili@hotmail.com','strili@strili')
 
-INSERT INTO Desejo (nomeDesejo, descricao)
-VALUES				('Namorado','Namorar o Gabriel Monteiro'),
-					('Playstation 5','Desejo de ter um Playstation 5'),
-					('PC GAMER','Desejo de ter um PC Gamer de última geração'),
-					('Carreira','Desejo de seguir carreira policial')
+INSERT INTO Desejo (idUsuario, nomeDesejo, descricao, dataCriacaoDesejo)
+VALUES				(1,'Namorado','Namorar o Gabriel Monteiro','27/05/2021'),
+					(1,'Playstation 5','Desejo de ter um Playstation 5','27/05/2021'),
+					(2,'PC GAMER','Desejo de ter um PC Gamer de última geração','27/05/2021'),
+					(2,'Playstation 5','Desejo de ter um Playstation 5','27/05/2021'),
+					(3,'Namorado','Namorar o Gabriel Monteiro','27/05/2021'),
+					(3,'PC GAMER','Desejo de ter um PC Gamer de última geração','27/05/2021'),
+					(4,'Namorado','Namorar o Gabriel Monteiro','27/05/2021'),
+					(4,'Carreira','Desejo de seguir carreira policial','27/05/2021')
 
